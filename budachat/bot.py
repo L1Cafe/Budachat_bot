9d87e9d107358167dfdbf4e44feebb7497cee986
L-Cafe <L-Cafe@linux.com>
2017-05-20 13:08:00 +0200

    init

---
+++ budachat/bot.py
@@ -0,0 +1,4 @@
+import requests
+import os                                           # For environmental variables
+from telegram import Bot as telegramBot
+from telegram import ParseMode as telegramParseMode
