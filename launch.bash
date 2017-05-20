9c660019aa3dd14c1dffc5b844f36325ce922fff
L-Cafe <L-Cafe@users.noreply.github.com>
2017-05-20 21:20:41 +0200

    bash launcher

---
+++ launch.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Telegram Bot token
+export telegramToken="<YOUR TELEGRAM TOKEN HERE>"
+# Telegram admin ID
+export telegramAdminId="<YOUR ID HERE>"
+# Telegram chat ID
+export telegramChatId="<YOUR GROUP ID HERE>"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+`which python3` budachat/bot.py
